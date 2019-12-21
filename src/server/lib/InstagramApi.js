c1353071acf545b7a04b33710e7f8a38a2addf93
Den Do <dendosg@gmail.com>
2019-12-21 15:34:33 +0700

    update request get user by id

--- src/server/lib/InstagramApi.js
+++ src/server/lib/InstagramApi.js
@@ -48,7 +48,8 @@ class Instagram {
       headers: {
         "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 12_3_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 Instagram 105.0.0.11.118 (iPhone11,8; iOS 12_3_1; en_US; en-US; scale=2.00; 828x1792; 165586599)",
         Cookie: this.credentials.cookie
-      }
+      },
+      json: true
     };
     return request(options)
       .then(body => body.user)
